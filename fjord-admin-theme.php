8bc21a1298060c38a53154888f1dc65d923cf95c
Joshua Vachon <46571227+joshuavachon25@users.noreply.github.com>
2023-07-02 06:24:22 -0400

    Create fjord-admin-theme.php

---
+++ fjord-admin-theme.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+Plugin Name: Fjord Admin Theme
+Plugin URI: http://fjordcms.app/
+Description: Simple wordpress admin theme for white labeling
+Author: Joshua Vachon
+Author URI: http://joshuavachon.dev/
+Version: 1.0.0
+*/
+
+
+
+%>
