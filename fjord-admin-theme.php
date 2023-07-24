b24e8427e6e9dfbb745a5ddb48d6302bc049683c
Joshua Vachon <46571227+joshuavachon25@users.noreply.github.com>
2023-07-23 21:02:30 -0400

    Update fjord-admin-theme.php

--- fjord-admin-theme.php
+++ fjord-admin-theme.php
@@ -11,27 +11,49 @@ Version: 1.0.0
 
 function get_fjord_css() {
   wp_enqueue_style( 'fjordcms-admin-theme', plugins_url('css/fjord.css', __FILE__));
+  wp_enqueue_script( 'fjord', plugins_url( "js/fjord.js", __FILE__ ), array() , '1.0.0' );
 }
 
 function set_fjord_footer() {
   return 'Propulsé par <a href="https://www.cyberfjord.studio/" target="_blank">Cyberfjord Studio </a>🚀';
 }
 
-// function set_theme_colors() {
-// 	$user_id = get_current_user_id();
-// 	$user_data = get_userdata($user_id);
-// 	if ( !( $user_data instanceof WP_User ) ) {
-// 		return;
-// 	}
-// 	$user_theme = $user_info->admin_color;
-//   include('css/' . $user_data . '.css');
-// }
+function get_userrole() {
+	$user_id = get_current_user_id();
+	$user_data = get_userdata($user_id);
+	if ( !( $user_data instanceof WP_User ) ) {
+		return;
+	}
+	$user_role = implode(', ', $user_data->roles);
+    return $user_role;
+}
+
+
+function admin_only(){
+	$role = get_userrole();
+	if($role !== "administrator"){
+		  remove_menu_page( 'jetpack' );                    //Jetpack* 
+		  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+		  remove_menu_page( 'edit-comments.php' );          //Comments
+		  remove_menu_page( 'themes.php' );                 //Appearance
+		  remove_menu_page( 'plugins.php' );                //Plugins
+		  remove_menu_page( 'users.php' );                  //Users
+		  remove_menu_page( 'tools.php' );                  //Tools
+		  remove_menu_page( 'options-general.php' );        //Settings
+		  remove_menu_page( 'litespeed');
+		  remove_menu_page( 'hostinger');
+	}
+
+}
 
 add_action( 'admin_enqueue_scripts', 'get_fjord_css' );
 add_action( 'login_enqueue_scripts', 'get_fjord_css' );
 add_filter('admin_footer_text', 'set_fjord_footer');
-// add_action( 'admin_head', 'set_theme_colors' );
-// add_action( 'login_head', 'set_theme_colors' );
+add_action( 'admin_init', 'admin_only' );
+// add_action( 'admin_init', 'wpse_136058_debug_admin_menu' );
 
+// function wpse_136058_debug_admin_menu() {
 
+//     echo '<pre>' . print_r( $GLOBALS[ 'menu' ], TRUE) . '</pre>';
+// }
 ?>
